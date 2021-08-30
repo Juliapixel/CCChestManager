5a077a910ea3b092503e2cc65bf71aea26d5551a
Juliapixel <juliapasc2004@gmail.com>
2021-08-30 00:06:37 -0300

    initial UI implementation

--- main.lua
+++ main.lua
@@ -1,9 +1,18 @@
 local store = require("apis.store")
 local retrieve = require("apis.retrieve")
 
-local args = {...}
-if args[1] == "store" then
-  store.storeItems(args[2])
-else
-  retrieve.retrieveItem(args[1], args[2], args[3])
-end
+local w, h = term.getSize()
+
+local header = window.create(term.current(), 1, 1, w, 1)
+local main = window.create(term.current(), 1, 2, w, h - 2)
+local chin = window.create(term.current(), 1, h, w, 1)
+
+header.setBackGroundColor(colors.blue)
+header.clear()
+header.write(" Store ")
+header.write("Retrieve")
+
+chin.setBackGroundColor(colors.gray)
+chin.clear()
+chin.setCursorPos(w - #"Settings" + 1, 1)
+chin.write("Settings")
